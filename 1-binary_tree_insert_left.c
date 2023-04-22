eb770477a405647f1191fb7eac37132bc17f2fd4
gumquat <gumquat@gmail.com>
2023-04-22 10:45:45 -0700

    fixed 0-2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,14 +1,12 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_insert_left - Inserts a node as the left-child
-  * of another node.
-  * @parent: A pointer to the node to insert the left-child in.
-  * @value: The value to store in the new node.
-  *
-  * Return: A pointer to the created node, or NULL on failure or
-  * if parent is NULL.
-  */
+* binary_tree_insert_left - puts a node as the left-child of another node
+* @parent: A pointer to the node to insert the left-child in
+* @value: The value to store in the new node
+* Return: pointer to new node, or NULL on failure / parent is NULL
+*/
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_left_node = NULL;
--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,14 +1,11 @@
 #include "binary_trees.h"
 
 /**
-  * binary_tree_insert_right - Inserts a node as the right-child
-  * of another node.
-  * @parent: A pointer to the node to insert the left-child in.
-  * @value: The value to store in the new node.
-  *
-  * Return: A pointer to the created node, or NULL on failure or
-  * if parent is NULL.
-  */
+* binary_tree_insert_right - puts a node as the right-child of another node
+* @parent: A pointer to the node to insert the left-child in
+* @value: The value to store in the new node
+* Return: A pointer to the created node - NULL on failure / parent is NULL.
+*/
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_right_node = NULL;
